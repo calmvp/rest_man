edd23396a77b8e626b1f436f1add92c7e4d5f86d
Ned KY <nkaidayip@gmail.com>
2013-09-24 22:40:36 -0500

    altered carts

--- app/assets/javascripts/restaurant/restaurant.js
+++ app/assets/javascripts/restaurant/restaurant.js
@@ -10,7 +10,7 @@ var Carts = {
       var test = $.each(data, function(k,v) {
         $.each(v, function(i, l){
           if($("#" + l.id + "").length == 0) {
-          $('.pending-carts').append('<li class="order"><a id="' + l.id + '" href="carts/' + l.id + '">' + 'Cart #'  + l.id + 'Table' + l.table.id + 'Seat' + l.seat.id +  '</a></li>')
+          $('.pending-carts').append('<li class="order"><a id="' + l.id + '" href="carts/' + l.id + '">' + 'Cart #'  + l.id + '</a></li>')
           }
         })
       })
